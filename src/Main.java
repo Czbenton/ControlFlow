23a695cf6f086d1cd5a2031cb5d078bf6eaa8f28
Zach Benton <Zach@Zach.local>
2016-10-19 15:36:16 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
 
-    public static Scanner scanner = new Scanner(System.in);  // this is a static constructor to make it available to ATM class
+    static Scanner scanner = new Scanner(System.in);
 
+    static private ATM atm = new ATM();
 
-    public static void main(String[] args) throws Exception{
-        ATM atm = new ATM();  //this does not need to be static since Main is the only method calling it
+    public static void main(String[] args) throws Exception {
 
-        atm.homeScreen();  //calling my homeScreen method
-        atm.menuSelect();  //calling my menuSelect method
+        atm.homeScreen();
+        while (cancelIsNotSelected()) {
+            atm.menuSelect();
+        }
+    }
 
-
+    private static boolean cancelIsNotSelected() {
+        return atm.option == null || !atm.option.equals("3");
     }
 }
